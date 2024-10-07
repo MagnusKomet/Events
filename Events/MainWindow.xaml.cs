b8a8c69b17d88e8f0fbd67007a98e7c25fa4c27f
MagnusKomet <ehosta.dam@institutcampalans.net>
2024-10-07 17:13:48 +0200

    Agregar archivos de proyecto.

---
+++ Events/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace Events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Has clicat el botó 1!!!!!!!!!!!!!!!! \nEs propaga");
+            //Trace.WriteLine("Text");
+            MessageBox.Show("Has clicat el botó 1!!!!!!!!!!!!!!!! \nEs propaga");
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Has clicat el botó 2!!!!!!!!!!!!!!!! \nEs propaga");
+            //Trace.WriteLine("Text");
+            MessageBox.Show("Has clicat el botó 2!!!!!!!!!!!!!!!! \nEs propaga");
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Has clicat el botó 3!!!!!!!!!!!!!!!! \nNo es propaga");
+            //Trace.WriteLine("Text");
+            MessageBox.Show("Has clicat el botó 3!!!!!!!!!!!!!!!! \nNo es propaga");
+            e.Handled = true;
+        }
+
+        /*
+        // Tunneling
+        private void Panel_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine("Has clicat l'stack !!!!!!!!!!!!!!!!");
+            MessageBox.Show("Has clicat l'stack !!!!!!!!!!!!!!!!");
+        }
+        */
+         
+        // Bubbling
+        private void Panel_Click(object sender, RoutedEventArgs e)
+        {
+
+            Debug.WriteLine("Has clicat l'stack !!!!!!!!!!!!!!!! \nOrigen:\n" + e.OriginalSource.ToString());
+            MessageBox.Show("Has clicat l'stack !!!!!!!!!!!!!!!! \nOrigen:\n" + e.OriginalSource.ToString());
+            
+        }
+        
+
+    }
+}
